84ea8cc92deab880615bde4497a4a3889c183265
Volha Rai <raivolha@gmail.com>
2025-12-06 23:42:24 +0100

    fix docker file

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5a23003e41a223c222e2bd892b967f5938c8f39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+980fef7ccb9b7070707a17de5a2781a120fbffcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
